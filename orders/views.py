cd317443994ec0a4648d4a4c2f1507e96441ecd0
Yassine Nifa <yassine.nifa@artex.io>
2022-10-03 21:37:36 +0200

    Test the rest framework using orders application
    Signed-off-by: Yassine Nifa <yassine.nifa@artex.io>

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from rest_framework import generics, status
+from rest_framework.response import Response
 # Create your views here.
+class HelloOrdersView(generics.GenericAPIView):
+    def get(self, request):
+        return Response(data={"message": "Hello Orders View"}, status = status.HTTP_200_OK)
