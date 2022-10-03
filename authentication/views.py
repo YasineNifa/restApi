7e3f74fc8458e62ce6cae602cd4dcec862764ec4
Yassine Nifa <yassine.nifa@artex.io>
2022-10-03 21:31:38 +0200

    Test the rest framework application
    Signed-off-by: Yassine Nifa <yassine.nifa@artex.io>

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,9 @@
+from sre_parse import State
+from telnetlib import STATUS
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloAuthView(generics.GenericAPIView):
+    def get(self, request):
+        return Response(data = {"message": "Hello Auth"}, status=status.HTTP_200_OK)
